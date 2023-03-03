4e0e8e1c6d369eefe0687cfba7a65816d95b0c75
weiguo.ni <neefrankie@gmail.com>
2023-03-03 14:12:23 +0800

    custom types

---
+++ custom_types/src/main.rs
@@ -0,0 +1,245 @@
+use crate::List::*;
+
+static LANGUAGE: &str = "Rust";
+const THRESHOLD: i32 = 10;
+
+fn is_big(n: i32) -> bool {
+    n > THRESHOLD
+}
+
+fn main() {
+    use crate::Status::{Poor, Rich};
+    use crate::Work::*;
+
+    let name = String::from("Peter");
+    let age = 27;
+    let peter = Person { name, age };
+
+    println!("{:?}", peter);
+
+    let point: Point = Point { x: 10.3, y: 0.4 };
+
+    println!("point coordinates: ({}, {})", point.x, point.y);
+
+    let bottom_right = Point { x: 5.2, ..point };
+
+    println!("second point: ({}, {})", bottom_right.x, bottom_right.y);
+
+    let Point {
+        x: left_edge,
+        y: top_edge,
+    } = point;
+
+    let rectangle = Rectangle {
+        top_left: Point {
+            x: left_edge,
+            y: top_edge,
+        },
+        bottom_right: Point { x: 15.5, y: -0.6 },
+    };
+
+    println!("{:?}", rectangle);
+
+    let _unit = Unit;
+
+    let pair = Pair(1, 0.1);
+
+    println!("pair contains {:?} and {:?}", pair.0, pair.1);
+
+    let Pair(integer, decimal) = pair;
+
+    println!("pair contains {:?} and {:?}", integer, decimal);
+
+    println!("{}", rect_area(rectangle));
+
+    println!("Sqaure {:?}", square(point, 3.0));
+
+    let pressed = WebEvent::KeyPress('x');
+    let pasted = WebEvent::Paste("my text".to_owned());
+    let click = WebEvent::Click { x: 20, y: 80 };
+    let load = WebEvent::PageLoad;
+    let unload = WebEvent::PageUnload;
+
+    inspect(pressed);
+    inspect(pasted);
+    inspect(click);
+    inspect(load);
+    inspect(unload);
+
+    let x = Operations::Add;
+
+    let status = Poor;
+    let work = Civilian;
+
+    match status {
+        Rich => println!("The rich have lots of money!"),
+        Poor => println!("The poor have no money..."),
+    }
+
+    match work {
+        Civilian => println!("Civilian work!"),
+        Soldier => println!("Solders fight!"),
+    }
+
+    println!("zero is {}", Number::Zero as i32);
+    println!("one is {}", Number::One as i32);
+
+    println!("roses are ${:06x}", Color::Red as i32);
+    println!("violets are #{:06x}", Color::Blue as i32);
+
+    let mut list = List::new();
+
+    list = list.prepend(1);
+    list = list.prepend(2);
+    list = list.prepend(3);
+
+    println!("linked list has length: {}", list.len());
+    println!("{}", list.stringify());
+
+    let n = 16;
+
+    println!("This is {}", LANGUAGE);
+    println!("The threshold is {}", THRESHOLD);
+    println!("{} is {}", n, if is_big(n) { "big" } else { "small" });
+}
+
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age: u8,
+}
+
+struct Unit;
+
+struct Pair(i32, f32);
+
+#[derive(Debug)]
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    top_left: Point,
+    bottom_right: Point,
+}
+
+fn rect_area(rec: Rectangle) -> f32 {
+    let Rectangle {
+        top_left: Point {
+            x: left_edge,
+            y: top_edge,
+        },
+        bottom_right: Point {
+            x: right_edge,
+            y: bottom_edge,
+        },
+    } = rec;
+
+    let width = right_edge - left_edge;
+    let height = top_edge - bottom_edge;
+
+    return width * height;
+}
+
+fn square(p: Point, l: f32) -> Rectangle {
+    return Rectangle {
+        top_left: Point { x: p.x, y: p.y },
+        bottom_right: Point {
+            x: p.x + l,
+            y: p.y - l,
+        },
+    };
+}
+
+enum WebEvent {
+    PageLoad,
+    PageUnload,
+    KeyPress(char),
+    Paste(String),
+    Click { x: i64, y: i64 },
+}
+
+fn inspect(event: WebEvent) {
+    match event {
+        WebEvent::PageLoad => println!("page loaded"),
+        WebEvent::PageUnload => println!("page unloaded"),
+        WebEvent::KeyPress(c) => println!("pressed '{}'.", c),
+        WebEvent::Paste(s) => println!("pasted \"{}\".", s),
+        WebEvent::Click { x, y } => {
+            println!("clicked at x={}, y={}.", x, y);
+        }
+    }
+}
+
+enum VeryVerboseEnumOfThingsToDoWithNumbers {
+    Add,
+    Subtract,
+}
+
+impl VeryVerboseEnumOfThingsToDoWithNumbers {
+    fn run(&self, x: i32, y: i32) -> i32 {
+        match self {
+            Self::Add => x + y,
+            Self::Subtract => x - y,
+        }
+    }
+}
+
+type Operations = VeryVerboseEnumOfThingsToDoWithNumbers;
+
+enum Status {
+    Rich,
+    Poor,
+}
+
+enum Work {
+    Civilian,
+    Soldier,
+}
+
+enum Number {
+    Zero,
+    One,
+    Two,
+}
+
+enum Color {
+    Red = 0xff0000,
+    Green = 0x00ff00,
+    Blue = 0x0000ff,
+}
+
+enum List {
+    Cons(u32, Box<List>),
+    Nil,
+}
+
+impl List {
+    fn new() -> List {
+        Nil
+    }
+
+    fn prepend(self, elem: u32) -> List {
+        Cons(elem, Box::new(self))
+    }
+
+    fn len(&self) -> u32 {
+        match *self {
+            Cons(_, ref tail) => 1 + tail.len(),
+            Nil => 0,
+        }
+    }
+
+    fn stringify(&self) -> String {
+        match *self {
+            Cons(head, ref tail) => {
+                format!("{}, {}", head, tail.stringify())
+            }
+            Nil => {
+                format!("Nil")
+            }
+        }
+    }
+}
