0ecaad569e5758ccdc5a08b152d6ceff38941f05
weiguo.ni <neefrankie@gmail.com>
2023-03-10 14:24:24 +0800

    std library

---
+++ std_lib/src/main.rs
@@ -0,0 +1,263 @@
+use std::{collections::HashMap, mem};
+
+mod checked {
+    #[derive(Debug)]
+    pub enum MathError {
+        DivisionByZero,
+        NonPositiveLogarithm,
+        NegativeSquareRoot,
+    }
+
+    pub type MathResult = Result<f64, MathError>;
+
+    pub fn div(x: f64, y: f64) -> MathResult {
+        if y == 0.0 {
+            Err(MathError::DivisionByZero)
+        } else {
+            Ok(x / y)
+        }
+    }
+
+    pub fn sqrt(x: f64) -> MathResult {
+        if x < 0.0 {
+            Err(MathError::NegativeSquareRoot)
+        } else {
+            Ok(x.sqrt())
+        }
+    }
+
+    pub fn ln(x: f64) -> MathResult {
+        if x <= 0.0 {
+            Err(MathError::NonPositiveLogarithm)
+        } else {
+            Ok(x.ln())
+        }
+    }
+
+    fn op_(x: f64, y: f64) -> MathResult {
+        let ratio = div(x, y)?;
+
+        let ln = ln(ratio)?;
+
+        sqrt(ln)
+    }
+
+    pub fn op(x: f64, y: f64) {
+        match op_(x, y) {
+            Err(why) => panic!(
+                "{}",
+                match why {
+                    MathError::NonPositiveLogarithm => "logarithm of non-positive number",
+                    MathError::DivisionByZero => "division by zero",
+                    MathError::NegativeSquareRoot => "square root of negative number",
+                }
+            ),
+            Ok(value) => println!("{}", value),
+        }
+    }
+}
+
+fn call(number: &str) -> &str {
+    match number {
+        "798-1364" => {
+            "We're sorry, the call cannot be completed as dialed. Please hang up and try again."
+        }
+        "645-7689" => {
+            "Hello, this is Mr. Awesome's Pizza. My name is Fred. What can I get for you today?"
+        }
+        _ => "Hi! Who is this again?",
+    }
+}
+
+fn main() {
+    // Box
+    let point: Point = origin();
+    let rectangle: Rectangle = Rectangle {
+        top_left: origin(),
+        bottom_right: Point { x: 3.0, y: -4.0 },
+    };
+
+    let boxed_rectangle: Box<Rectangle> = Box::new(Rectangle {
+        top_left: origin(),
+        bottom_right: Point { x: 3.0, y: -4.0 },
+    });
+
+    let boxed_point: Box<Point> = Box::new(origin());
+
+    let box_in_a_box: Box<Box<Point>> = Box::new(boxed_origin());
+
+    println!(
+        "Point occupies {} bytes on the stack",
+        mem::size_of_val(&point)
+    );
+    println!(
+        "Rectangle occupies {} bytes on the stack",
+        mem::size_of_val(&rectangle)
+    );
+
+    println!(
+        "Boxed point occupies {} bytes on the stack",
+        mem::size_of_val(&boxed_point)
+    );
+    println!(
+        "Boxed rectangle occupies {} bytes on the stack",
+        mem::size_of_val(&boxed_rectangle)
+    );
+    println!(
+        "Boxed box occupies {} bytes on the stack",
+        mem::size_of_val(&box_in_a_box)
+    );
+
+    let unboxed_point: Point = *boxed_point;
+    println!(
+        "Unboxed point occupies {} bytes on the stack",
+        mem::size_of_val(&unboxed_point)
+    );
+
+    // Vectors
+    let collected_iterator: Vec<i32> = (0..10).collect();
+    println!("Collected (0..10) into: {:?}", collected_iterator);
+
+    let mut xs = vec![1i32, 2, 3];
+    println!("Initial vector: {:?}", xs);
+
+    println!("Push 4 into the vector");
+    xs.push(4);
+    println!("Vector: {:?}", xs);
+
+    println!("Vector length: {}", xs.len());
+
+    println!("Second element: {}", xs[1]);
+
+    println!("Pop last element: {:?}", xs.pop());
+
+    // println!("Fourth element: {}", xs[3]);
+
+    println!("Contents of xs:");
+    for x in xs.iter() {
+        println!("> {}", x);
+    }
+
+    for (i, x) in xs.iter().enumerate() {
+        println!("In position {} we have value {}", i, x);
+    }
+
+    for x in xs.iter_mut() {
+        *x *= 3
+    }
+    println!("Updated vector: {:?}", xs);
+
+    // Strings
+    let pangram: &'static str = "the quick brown fox jumps over the lazy dog";
+    println!("Pangram: {}", pangram);
+
+    println!("Words in reverse");
+    for word in pangram.split_whitespace().rev() {
+        println!("> {}", word);
+    }
+
+    let mut chars: Vec<char> = pangram.chars().collect();
+    chars.sort();
+    chars.dedup();
+    println!("{:?}", chars);
+
+    let mut string = String::new();
+    for c in chars {
+        string.push(c);
+        string.push_str(", ");
+    }
+    println!("{}", string);
+
+    let chars_to_trim: &[char] = &[' ', ','];
+    let trimmed_str: &str = string.trim_matches(chars_to_trim);
+    println!("Used characters: {}", trimmed_str);
+
+    let alice = String::from("I like dogs");
+    let bob: String = alice.replace("dog", "cat");
+
+    println!("Alice says: {}", alice);
+    println!("Bob says: {}", bob);
+
+    // Opiton
+    try_division(4, 2);
+    try_division(1, 0);
+
+    let none: Option<i32> = None;
+    let _equivalent_none = None::<i32>;
+
+    let optional_float = Some(0f32);
+
+    // println!(
+    //     "{:?} unwrap to {:?}",
+    //     optional_float,
+    //     optional_float.unwrap()
+    // );
+
+    // println!("{:?} unwraps to {:?}", none, none.unwrap());
+
+    // Result
+    // checked::op(1.0, 10.0);
+
+    // HashMap
+    let mut contacts = HashMap::new();
+    contacts.insert("Daniel", "798-1364");
+    contacts.insert("Ashley", "645-7689");
+    contacts.insert("Katie", "435-8291");
+    contacts.insert("Robert", "956-1745");
+
+    match contacts.get(&"Daniel") {
+        Some(&number) => println!("Calling Daniel: {}", call(number)),
+        _ => println!("Don't have Daniel's number."),
+    }
+
+    contacts.insert("Daniel", "164-6743");
+
+    match contacts.get(&"Ashley") {
+        Some(&number) => println!("Calling Ashley: {}", call(number)),
+        _ => println!("Don't have Ashley's number."),
+    }
+
+    contacts.remove("Ashley");
+
+    for (contact, &number) in contacts.iter() {
+        println!("Calling {}: {}", contact, call(number));
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy)]
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+#[allow(dead_code)]
+struct Rectangle {
+    top_left: Point,
+    bottom_right: Point,
+}
+
+fn origin() -> Point {
+    Point { x: 0.0, y: 0.0 }
+}
+
+fn boxed_origin() -> Box<Point> {
+    Box::new(Point { x: 0.0, y: 0.0 })
+}
+
+fn checked_division(dividend: i32, divisor: i32) -> Option<i32> {
+    if divisor == 0 {
+        None
+    } else {
+        Some(dividend / divisor)
+    }
+}
+
+fn try_division(dividend: i32, divisor: i32) {
+    match checked_division(dividend, divisor) {
+        None => println!("{} / {} failed", dividend, divisor),
+        Some(quotient) => {
+            println!("{} / {} = {}", dividend, divisor, quotient)
+        }
+    }
+}
